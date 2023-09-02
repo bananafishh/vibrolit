37a2a827bd5b667f33d1e587e3d101bb4dbe3589
Зули Дышекова <bananafishh@Zulis-MacBook-Pro.local>
2023-09-02 22:23:30 +0300

    Завершение работ по реализации адаптивности

---
+++ source/blocks/slider/slider.js
@@ -0,0 +1,24 @@
+const slider = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        // when window width is >= 710px
+        710: {
+          slidesPerView: 2,
+        },
+        // when window width is >= 1200px
+        1200: {
+          slidesPerView: 3,
+        },
+    },
+    navigation: {
+        nextEl: '.slider__button-next',
+        prevEl: '.slider__button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        dynamicMainBullets: 3,
+    },
+});
