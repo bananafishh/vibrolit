70416dc370c7d648dbab860989f839b66c824d12
Zuli Dyshekova <z.dyshekova@gmail.com>
2023-09-26 16:47:15 +0300

    Реализация плавной прокрутки к разделам документа с помощью JavaScript

--- source/blocks/main-nav/main-nav.js
+++ source/blocks/main-nav/main-nav.js
@@ -2,11 +2,10 @@
 
 (function () {
     const mainNav = document.querySelector('.js-main-nav');
-    
-    mainNav.addEventListener('click', event => {
-        const activeMainNavLink = document.querySelector('.main-nav__link--active');
-        const currentMainNavLink = event.target;
 
+    function addActiveClass(currentMainNavLink) {
+        const activeMainNavLink = document.querySelector('.main-nav__link--active');
+        
         if (!currentMainNavLink.classList.contains('main-nav__link--active')) {
             if (activeMainNavLink) {
                 activeMainNavLink.classList.remove('main-nav__link--active');
@@ -14,5 +13,24 @@
 
             currentMainNavLink.classList.add('main-nav__link--active');
         }
+    }
+
+    function scrollToTargetSection(event, currentMainNavLink) {
+        event.preventDefault();
+
+        const targetSectionPosition = document.querySelector(`${currentMainNavLink.hash}`).getBoundingClientRect().top + window.scrollY;
+        const stickyPageHeaderHeight = document.querySelector('.js-page-header').clientHeight;
+    
+        window.scrollTo({
+            top: targetSectionPosition - stickyPageHeaderHeight,
+            behavior: 'smooth',
+       });
+    }
+    
+    mainNav.addEventListener('click', event => {
+        const currentMainNavLink = event.target;
+
+        addActiveClass(currentMainNavLink);
+        scrollToTargetSection(event, currentMainNavLink);
     });
 })();
